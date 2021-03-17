afa1491b13ce79d7682502f5b245a766f4f8ae89
smerton <simonmerton@hotmail.co.uk>
2021-03-17 21:41:34 +0000

    Start work on a new mesh generator.

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// A simple mesh generator for DG problem input
+
+// Author S. R. Merton
+
+#include <iostream>
+
+using namespace std;
+
+
+// main function starts here
+
+int main(){
+
+  cout<<"main(): Starting up main loop..."<<endl;
+
+  cout<<"Normal termination."<<endl;
+
+  return 0;
+}
