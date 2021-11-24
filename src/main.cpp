20b698d6fc87562ada25d31af9e41b04e47f7242
smerton <simonmerton@hotmail.co.uk>
2021-11-24 11:35:45 +0000

    Add command line arguments for input and output files.

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,25 @@ using namespace std;
 
 // main function starts here
 
-int main(){
+int main(int argc, char** argv){
+
+  if(argc!=3){
+    cout<<"main(): please specify an input file and an output file."<<endl;
+    exit(1);
+  }
+
+  cout<<"main(): input file: "<<argv[1]<<endl; // note [0] is the name of the program
+  cout<<"main(): output file: "<<argv[2]<<endl;
 
   cout<<"main(): Starting up main loop..."<<endl;
 
 // initialise a new input from file
 
-  Input I((char*)"input.gen");
+  Input I((char*) argv[1]);
 
 // write the mesh file
 
-  Output((char*) "input.mesh",&I);
+  Output((char*) argv[2],&I);
 
   cout<<"Normal termination."<<endl;
 
